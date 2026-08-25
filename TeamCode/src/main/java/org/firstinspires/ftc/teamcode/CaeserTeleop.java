c6ed1df030a225b9976b9e6287ec79b95d4778b9
Trinity Greenlawn Robotics <117218109+TitansFTC@users.noreply.github.com>
2026-08-25 19:47:43 -0400

    Implemented Dashboard Framework

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CaeserTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CaeserTeleop.java
@@ -1,16 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
-//import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 
 @TeleOp(name= "CaeserTeleop", group= "Titans TeleOps")
 
 public class  CaeserTeleop extends OpMode {
     private DriveBase driveBase = null;
-
+    private FtcDashboard dashboard = null;
+    private Telemetry dashboardTelemetry = null;
 public void init() {
     driveBase = new DriveBase(hardwareMap);
+
+
+    dashboard = FtcDashboard.getInstance();
+    dashboardTelemetry = dashboard.getTelemetry();
+
+    telemetry.addData("Status", "Initialized");
 }
 @Override
     public void loop()   {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase.java
@@ -12,6 +12,7 @@ public class DriveBase {
     public static final double FAST_POWER_FRACTION = 1.0;
         public static final double SLOW_POWER_FRACTION = 0.2;
         public static final double HALF_POWER_FRACTION = 0.5;
+    public static final double[] TARGET_REACHED = {0, 0, 0, 0, 0};
 
         private DcMotorEx leftFront = null;
         private DcMotorEx leftBack = null;
@@ -44,6 +45,74 @@ public class DriveBase {
 
        }
 
+    public boolean odoMove(double tar_pos_x, double tar_pos_y, double tar_angle, double cur_pos_x, double cur_pos_y, double cur_angle) {
+        double[] powers = odoCompute(tar_pos_x, tar_pos_y, tar_angle, cur_pos_x, cur_pos_y, cur_angle);
+
+        leftFront.setPower(powers[0] * powers[4]);
+        leftBack.setPower(powers[1] * powers[4]);
+        rightFront.setPower(powers[2] * powers[4]);
+        rightBack.setPower(powers[3] * powers[4]);
+
+        return powers == TARGET_REACHED;
+    }
+
+    public double[] odoCompute(double tar_pos_x, double tar_pos_y, double tar_angle, double cur_pos_x, double cur_pos_y, double cur_angle){
+        double rel_tar_x = tar_pos_x - cur_pos_x;
+        double rel_tar_y = tar_pos_y - cur_pos_y;
+
+        // Skip if we have reached target
+        if ((rel_tar_x == 0) && (rel_tar_y == 0) && (tar_angle == cur_angle)) {
+            return TARGET_REACHED;
+        }
+
+        double beta = 90;
+        if (rel_tar_y < 0) {
+            beta = -90;
+        }
+        if (rel_tar_x != 0) {
+            beta = Math.toDegrees(Math.atan(rel_tar_y / rel_tar_x));
+        }
+        if (rel_tar_x < 0) {
+            beta = beta - 180;
+        }
+
+        double pos_angle = 90 + cur_angle - beta;
+        double rel_pos_x = Math.sin(Math.toRadians(pos_angle));
+        double rel_pos_y = Math.cos(Math.toRadians(pos_angle));
+        double rel_angle = 0;
+
+        if (Math.abs(cur_angle - tar_angle) >= 30) {
+            if (tar_angle > cur_angle) {
+                rel_angle =  0.5;
+            } else {
+                rel_angle = -0.5;
+            }
+        } else {
+            rel_angle = ((tar_angle - cur_angle) / 30) * 0.5;
+        }
+
+        double powerFraction = FAST_POWER_FRACTION;
+        double distanceAway = Math.sqrt(rel_pos_x*rel_pos_x + rel_pos_y*rel_pos_y);
+        if (distanceAway < 6.0) {
+            powerFraction = Math.max(SLOW_POWER_FRACTION, distanceAway / 10);
+        }
+
+        double lfp = ((rel_pos_y + -rel_pos_x) - rel_angle);
+        double lbp = ((rel_pos_y +  rel_pos_x) - rel_angle);
+        double rfp = ((rel_pos_y +  rel_pos_x) + rel_angle);
+        double rbp = ((rel_pos_y + -rel_pos_x) + rel_angle);
+
+        if ((Math.abs(lfp) >= 1) || (Math.abs(rfp) >= 1) || (Math.abs(lbp) >= 1) || (Math.abs(rbp) >= 1)) {
+            double highestPower = Math.max(Math.max(Math.abs(lfp), Math.abs(rfp)), Math.max(Math.abs(rbp), Math.abs(lbp)));
+            lfp = lfp / highestPower;
+            lbp = lbp / highestPower;
+            rfp = rfp / highestPower;
+            rbp = rbp / highestPower;
+        }
+
+        double[] powers = {lfp, lbp, rfp, rbp, powerFraction};
+        return powers;
+    }
 
     public void stop () {
         leftFront.setPower(0);
